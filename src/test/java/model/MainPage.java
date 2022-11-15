c040549da66e947a1c7314bf0b62cbb913519065
Sabina Saad <sabinasaad@MacBook-Air-Sabina.local>
2022-11-15 12:40:50 +0300

    added locators, added description and plan

--- src/test/java/model/MainPage.java
+++ src/test/java/model/MainPage.java
@@ -1,9 +1,25 @@
 package model;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class MainPage extends BasePage {
 
+    @FindBy (id = "loginEmail")
+    private WebElement loginEmailField;
+
+    @FindBy (id = "loginPassword")
+    private WebElement loginPasswordField;
+
+    @FindBy (xpath = "//div[@id='authPage']//div/button")
+    private WebElement authSubmitButton;
+
+    @FindBy (id = "invalidEmailPassword")
+    private WebElement invalidDataErrorMessage;
+
+    @FindBy (id = "emailFormatError")
+    private WebElement emailFormatErrorMessage;
 
     public MainPage(WebDriver driver) {
         super(driver);
